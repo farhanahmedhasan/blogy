fe7c340c29899ed97c438accec46f365d8754600
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-12-05 03:13:33 +0600

    Added nav on posts and removed dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,35 +2,45 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex flex-1">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('post.index') }}">
                         <img src="{{ asset('images/appLogo.svg') }}"
                             class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    {{-- @if (Auth::user())
-                        <x-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')">
-                            {{ __('Your Posts') }}
-                        </x-nav-link>
-                    @endif --}}
-                </div>
+                @guest
+                    <div class="flex flex-1 justify-end">
 
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    {{-- @if (Auth::user())
-                        <x-nav-link :href="route('post.show')" :active="request()->routeIs('post.show')">
-                            {{ __('Create Post') }}
-                        </x-nav-link>
-                    @endif --}}
-                </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                                {{ __('Register') }}
+                            </x-nav-link>
+                        </div>
+
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
+                            <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                                {{ __('Login') }}
+                            </x-nav-link>
+
+
+                        </div>
+                    </div>
+                @endguest
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                @auth
+                    <div class="h-12 w-12 bg-white rounded-full overflow-hidden">
+                        <img class="h-full w-full object-cover" src="{{ asset('images/appLogo.svg') }}" alt="">
+                    </div>
+                @endauth
+
                 @if (Auth::user())
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
@@ -87,11 +97,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,9 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [PostController::class, 'index'])->name('post.index');
+// Route::get('/posts/{slug}', [PostController::class, 'show',])->name('post.show');
 
-Route::get('/posts', [PostController::class, 'index'])->name('post.index');
-Route::get('/posts/{slug}', [PostController::class, 'show',])->name('post.show');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

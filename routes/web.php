3c4062ed4ef641a75ee47ccdfc22209aad20efaf
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-12-04 22:53:44 +0600

    NAv changed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+Route::get('/posts/{slug}', [PostController::class, 'show',])->name('post.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

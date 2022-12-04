f5def660c315555a560876c4d2484892d4741c00
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-12-04 16:04:23 +0600

    Wrote postModel fillable and postRealationship with  user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'excerpt',
+        'body',
+        'min_to_read',
+        'likes'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
